1bc9c44710cf3b410c4b52180dfbb17809fc26a3
ev4host2000 <141960019+ev4host2000@users.noreply.github.com>
2026-04-29 16:19:30 +0300

    Create Purchase model

---
+++ lib/models/purchase.dart
@@ -0,0 +1,8 @@
+class Purchase {
+  final String id;
+  final DateTime date;
+  final double amount;
+  final String supplier;
+
+  Purchase({required this.id, required this.date, required this.amount, required this.supplier});
+}
