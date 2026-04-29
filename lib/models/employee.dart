6ae3a3d619bc7e95bf5c3975f14a67ac329163ac
ev4host2000 <141960019+ev4host2000@users.noreply.github.com>
2026-04-29 16:19:39 +0300

    Add Employee model to lib/models/employee.dart

---
+++ lib/models/employee.dart
@@ -0,0 +1,9 @@
+class Employee {
+  final String id;
+  final String name;
+  final double salary;
+  final String position;
+  final DateTime hireDate;
+
+  Employee({required this.id, required this.name, required this.salary, required this.position, required this.hireDate});
+}
