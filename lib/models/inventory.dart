d8733c549d1d003d6fe1a30ebfbc1ddec55c472f
ev4host2000 <141960019+ev4host2000@users.noreply.github.com>
2026-04-29 16:20:03 +0300

    Create Inventory model in lib/models/inventory.dart

---
+++ lib/models/inventory.dart
@@ -0,0 +1,8 @@
+class Inventory {
+  final String id;
+  final String productId;
+  final int quantity;
+  final DateTime lastUpdated;
+
+  Inventory({required this.id, required this.productId, required this.quantity, required this.lastUpdated});
+}
