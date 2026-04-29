d9e9cf339d4828f728dc8b2db51f17e52bec6f36
ev4host2000 <141960019+ev4host2000@users.noreply.github.com>
2026-04-29 16:01:45 +0300

    Create main.dart file as an entry point for Flutter accounting application with GetX setup and basic theme configuration

---
+++ main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Accounting App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Accounting App!'),
+      ),
+    );
+  }
+}
